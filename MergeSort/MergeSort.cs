208e0549604b662af70922433ac7dfc0e9ed1158
Matt Herron <birdmanherron@gmail.com>
2015-02-18 11:46:13 -0500

    Added merge sort project.

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    public static class MergeSort
+    {
+        public static int[] Sort(int[] toSort)
+        {
+            return Worker(toSort, 0, toSort.Length - 1);
+        }
+
+        private static int[] Worker(int[] toSort, int begin, int end)
+        {
+            if (end == begin)
+                return new[] {toSort[begin]};
+
+            var middle = ((end - begin) / 2) + begin;            
+            var left = Worker(toSort, begin, middle);
+            var right = Worker(toSort, middle + 1, end);
+            return Merge(left, right);
+        }
+
+        private static int[] Merge(int[] a, int[] b)
+        {
+            var ai = 0;
+            var bi = 0;
+            var toReturn = new int[a.Length + b.Length];
+
+            for (var i = 0; i < toReturn.Length; i++)
+            {
+                if (ai == a.Length)
+                    toReturn[i] = b[bi++];
+                else if (bi == b.Length)
+                    toReturn[i] = a[ai++];
+                else
+                    toReturn[i] = a[ai] < b[bi] ? a[ai++] : b[bi++];
+            }
+
+            return toReturn;
+        }
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toSort = new[] {5, 6, 2, 3, 1, 8, 9, 10, 4, 7};
+            Console.Write("Before sort: ");
+            PrintArray(toSort);
+            Console.WriteLine();
+            var sorted = MergeSort.Sort(toSort);
+            Console.Write("After sort: ");
+            PrintArray(sorted);
+            Console.ReadLine();
+        }
+
+        private static void PrintArray(int[] toPrint)
+        {
+            foreach (var i in toPrint)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
